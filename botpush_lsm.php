406335842221fd0602abd07249b140fe12d38979
maxdigz <doogdigg@gmail.com>
2018-10-04 04:47:17 +0700

    n

--- botpush_lsm.php
+++ botpush_lsm.php
@@ -23,7 +23,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("LSM99\n\nแจ้งถอนเงิน\n\nวันที่ทำรายการ : ".$date."\n\ncallcenter : ".$callcenter."\nLSMID : ".$lsmid."\nจำนวนเงินที่ถอน : ".$amount." บาท\nชื่อบัญชี : ".$accname."\nเลขที่บัญชี : ".$accno."\nธนาคาร : ".$bank);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("LSM99\nแจ้งถอนเงิน\nวันที่ทำรายการ : ".$date."\ncallcenter : ".$callcenter."\nLSMID : ".$lsmid."\nจำนวนเงินที่ถอน : ".$amount." บาท\nชื่อบัญชี : ".$accname."\nเลขที่บัญชี : ".$accno."\nธนาคาร : ".$bank);
 
 
 
