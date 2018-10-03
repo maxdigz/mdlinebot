be1b7d992f5b1898cbc6390147e94872fcd4cd19
maxdigz <doogdigg@gmail.com>
2018-10-04 03:23:48 +0700

    re3

--- webhookre.php
+++ webhookre.php
@@ -13,7 +13,7 @@
     if($message == "สวัสดี"){
         $arrayPostData['to'] = $id;
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา->>".$id;;
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "2";
         $arrayPostData['messages'][1]['stickerId'] = "34";
