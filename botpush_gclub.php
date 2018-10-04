e452cf69ab8ab4ef1ea3126ae3b1ae30f06ab47d
maxdigz <doogdigg@gmail.com>
2018-10-04 15:17:49 +0700

    a

--- botpush_gclub.php
+++ botpush_gclub.php
@@ -15,9 +15,12 @@ require "vendor/autoload.php";
 $access_token = 'BGFqduH48YAAWc9FYOu1FK12ah2JlNzho9UGguKqdpyBsHD8d9iE7KZ8gVPzESWqpfP5sDOeW9hRoXbZ7Y6VbSRGJZvs42x4BSsOOpdpf6bp3aTUYl8jN4dAsmRlO7o3jdPvoIzZXCOkjF1qHjZfuQdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '6be6eb0f43914264279b926499d5d59a';
+//test
+//$pushID = 'Uc9c84f548b5a45f88a0392ddb018529c';
 
-$pushID = 'Uc9c84f548b5a45f88a0392ddb018529c';
-
+    //prod
+$pushID = 'Cc96dc4b1e62fa26a177d5586dd4c38cc';
+    
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
