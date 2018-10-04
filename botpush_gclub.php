fd8261560daf22ffc667fec3c0eb921e3be7d19f
maxdigz <doogdigg@gmail.com>
2018-10-04 15:06:11 +0700

    gclub

---
+++ botpush_gclub.php
@@ -0,0 +1,42 @@
+<?php
+$id = $_GET['id'];
+$callcenter = $_GET['callcenter'];
+$gclubid = $_GET['gclubid'];
+$amount =$_GET['amount'];
+$accname = $_GET['accname'];
+$accno = $_GET['accno'];
+$bank = $_GET['bank'];
+
+$date = $_GET['date'];
+
+
+require "vendor/autoload.php";
+
+$access_token = 'BGFqduH48YAAWc9FYOu1FK12ah2JlNzho9UGguKqdpyBsHD8d9iE7KZ8gVPzESWqpfP5sDOeW9hRoXbZ7Y6VbSRGJZvs42x4BSsOOpdpf6bp3aTUYl8jN4dAsmRlO7o3jdPvoIzZXCOkjF1qHjZfuQdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = '6be6eb0f43914264279b926499d5d59a';
+
+$pushID = 'Uc9c84f548b5a45f88a0392ddb018529c';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("LSM99\nแจ้งถอนเงิน\nวันที่ทำรายการ : ".$date."\ncallcenter : ".$callcenter."\nLSMID : ".$lsmid."\nจำนวนเงินที่ถอน : ".$amount." บาท\nชื่อบัญชี : ".$accname."\nเลขที่บัญชี : ".$accno."\nธนาคาร : ".$bank);
+
+
+
+
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+//echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+//$url = "Location:http://localhost/supportmd/public/lsm99/".$id."/show";
+$url = "Location:http://support.gclubmd.com/lsm99/".$id."/show";
+
+header($url);
+
+
+
+
+
