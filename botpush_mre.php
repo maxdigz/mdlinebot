1848cca211c915b8112f06f345e4b60250166d38
maxdigz <43826974+maxdigz@users.noreply.github.com>
2019-10-17 02:30:13 +0700

    Update botpush_mre.php

--- botpush_mre.php
+++ botpush_mre.php
@@ -36,9 +36,9 @@ $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 //$url = "Location:http://localhost/supportmd/public/gclub/".$id."/show";
-$url = "Location:http://support2md.com/gclub/".$id."/show";
+//$url = "Location:http://support2md.com/gclub/".$id."/show";
 
-header($url);
+//header($url);
 
 
 
